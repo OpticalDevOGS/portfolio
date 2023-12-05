dc68c6410c1bad32667d6963687f98fa3fc4e19a
Opticaldevogs <cubylive@gmail.com>
2023-12-05 15:32:48 -0700

    JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,29 @@
 // Get the button element
-const button = document.getElementsByClassName('.headingButton');
-console.log("tis working");
-if (button != null) {
-    setTimeout(() => {
-        button.value = "meh";
-        console.log("tis working");
-    }, 2000);
-}
+var birthdayDate = new Date("2009-05-21");
+// const button = document.getElementsByClassName('.headingButton');
+// console.log("tis working");
+// if (button != null) {
+//     setTimeout(() => {
+//         button.value = "meh";
+//         console.log("tis working");
+//     }, 2000);
+// }
 
-window.onload = function() {
-    document.getElementsByClassName('.headerButton').className += "loaded";
-}
+// window.onload = function() {
+//     document.getElementsByClassName('.headerButton').className += "loaded";
+// }
 
-button.value = "meh";
+// button.value = "meh";
+
+function ageCalculation() {
+    var ageDifference = Date.now() - birthdayDate;
+    var DateofAge = new Date(ageDifference);
+    // document.getElementById("ageDiv").innerHTML = "("+(DateofAge.getUTCFullYear() - 1970)+" years old)";
+    document.getElementById("ageDiv").innerHTML = "("+(DateofAge.getUTCFullYear() - 1970)+" years old)";
+}
+setTimeout(() => {
+    ageCalculation();
+}, 2000);
 
 // // Add a mouseover event listener
 // button.addEventListener('mouseover', () => {
