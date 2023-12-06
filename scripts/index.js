89f882aec587057be3d97a76f72f9a23cd6b3ac9
opticaldevogs <cubylive@gmail.com>
2023-12-06 12:43:43 -0700

    Changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,29 @@
-// Get the button element
 var birthdayDate = new Date("2009-05-21");
-// const button = document.getElementsByClassName('.headingButton');
-// console.log("tis working");
-// if (button != null) {
-//     setTimeout(() => {
-//         button.value = "meh";
-//         console.log("tis working");
-//     }, 2000);
-// }
-
-// window.onload = function() {
-//     document.getElementsByClassName('.headerButton').className += "loaded";
-// }
-
-// button.value = "meh";
+const description = "Hello, I'm a JS, CSS and HTML developer that also does game devleopment.";
 
 function ageCalculation() {
     var ageDifference = Date.now() - birthdayDate;
     var DateofAge = new Date(ageDifference);
-    // document.getElementById("ageDiv").innerHTML = "("+(DateofAge.getUTCFullYear() - 1970)+" years old)";
     document.getElementById("ageDiv").innerHTML = "("+(DateofAge.getUTCFullYear() - 1970)+" years old)";
 }
-setTimeout(() => {
-    ageCalculation();
-}, 2000);
 
-// // Add a mouseover event listener
-// button.addEventListener('mouseover', () => {
-//   // Change the button's background color
-//   button.style.color = 'blue';
-// });
+function typewritter(element, text, i=0) {
+    element.textContent += text[i];
 
-// // Add a mouseout event listener
-// button.addEventListener('mouseout', () => {
-//   // Change the button's background color back to its original color
-//   button.style.color = '';
-// });
+    if (i==text.length -1) {
+        return;
+    }
+
+    setTimeout(() => typewritter(element, text, i+1), Math.random() * 100);
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        ageCalculation();
+       // while (true) {
+            //console.log(window.scrollY);
+        //}
+        typewritter(document.querySelector(".cole-galeDesc"), description);
+        console.log(window.scrollY);
+    }
+};
